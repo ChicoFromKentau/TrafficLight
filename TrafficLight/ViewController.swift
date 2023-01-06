385956777f26ddf31e247a461c1638ed67cace58
ChicoFromKentau <chingiz.alimov@gmail.com>
2023-01-06 15:46:56 +0600

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Chingiz Alimov on 05.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
