321c3f1c854d70d5bee01231d342bcffb6ed4951
ChicoFromKentau <chingiz.alimov@gmail.com>
2023-01-06 20:01:09 +0600

    Logic setup

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,16 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
 
+    @IBOutlet var button: UIButton!
     
+    private var currentLight = CurrentLight.red // задали первоначальную позицию
+    
+// для читаемости кода можно создать две приватные переменные
+//    private var lightIsOn: CGFloat = 1
+//    private var lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // скругляем углы кнопки
+        button.layer.cornerRadius = 11
         
+        // выключаем все три лампы
+        redLightView.alpha = 0.3 // или же = lightIsOff
+        yellowLightView.alpha = 0.3 // или же = lightIsOff
+        greenLightView.alpha = 0.3 // или же = lightIsOff
         
+        // скругляем углы динамических вьюшек
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.bounds.width / 2
     }
 
-
+    @IBAction func buttonDidTap() {
+        
+        // заменяем текст кнопки при нажатии
+        if button.currentTitle == "НАЧАТЬ" {
+            button.setTitle("ПЕРЕКЛЮЧИТЬ", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLightView.alpha = 1 // или же = lightIsOn
+            greenLightView.alpha = 0.3 // или же = lightIsOff
+            currentLight = .yellow
+        case .yellow:
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+            currentLight = .green
+        case .green:
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            currentLight = .red
+        }
+    }
 }
 
+// MARK: CurrentLight
+extension ViewController {
+    enum CurrentLight {
+        case red, yellow, green
+    }
+}
